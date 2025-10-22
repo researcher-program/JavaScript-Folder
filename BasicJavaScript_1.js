578e34903ed42350e63058b6e84462f009bb3a82
AL - ARAFAT (  YEASH  ) <149018537+researcher-program@users.noreply.github.com>
2025-10-23 03:18:15 +0600

    Add files via upload
    This repo is " JavaScript Variable Defination and Details , Syntax " for beginners.

---
+++ BasicJavaScript_1.js
@@ -0,0 +1,79 @@
+// Ryan Dahl is the founder of Node.Js and Deno.
+
+/* Variable => 3 types of Variables { const[not changeable],let[more useable in js] and var[it's better not to use] } */
+
+// Variable Define{ var, let, const }
+
+/* 
+Prefer to not use of " var --Variable " ,because of in issues in "block and functional scope" . "Use strict!";Treat all JS code as newer version.
+*/
+
+/*
+var name="Arafat"
+alert(name);
+var first_name="Al-"
+var last_name="Arafat"
+alert(first_name + last_name);
+*/
+
+/*
+alert( 3 + 3);
+ We are using node.js,not browser.
+*/
+
+/*
+alert ('Allah Is Great.');
+console.log ('Allah is great');
+document.write ("Allah blesses you");
+document.write ("<h1>Allah blesses you</h1>");
+document.getElementById("May Allah  bless you");
+document.getElementById("demo");
+*/
+/*
+ name1="Arafat";
+ cell='01754545335';
+email='alarafatyeashgmail.com';
+website="www.arafat.com"; 
+var name1="Arafat";
+var cell='01754545335';
+var email='alarafatyeashgmail.com';
+var website="www.arafat.com";
+let name1="Arafat";
+let cell='01754545335';
+let email='alarafatyeashgmail.com';
+let website="www.arafat.com";
+const name1="Arafat";
+const cell='01754545335';
+const email='alarafatyeashgmail.com';
+const website="www.arafat.com"; 
+
+console.log("Person Name"+name1+'Person Cell'+cell+email+website);
+*/
+
+// const { StrictMode } = require("react");
+
+                        // Main Parts Start Here...
+const accountId = 15344
+accountId = 2 //not allowed for constant,because it's not changeable.
+let accountEmail = " arafatgmail.com "
+accountEmail = " arfangmail.com "
+var accountPassword = " 1545645 "
+ accountPassword = " 1545 "
+accountCity = "Demra"
+ accountCity = " Dhaka "
+
+console.log(accountId); //It's used to "Execute" this program.
+console.log("Arafat");
+/*
+console.log(3
+     +
+     3);
+*/
+// Please stay out of this kind of code,because of  readability should be high.
+
+
+ let accountState; //Undefined
+console.table( [ accountEmail, accountPassword, accountCity, accountState ] );
+
+
+
