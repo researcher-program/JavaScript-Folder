66cfb0671e99929a4b847395f09a86423b6b5711
AL - ARAFAT (  YEASH  ) <149018537+researcher-program@users.noreply.github.com>
2025-12-31 14:56:08 +0600

    Add files via upload

---
+++ test-11.js
@@ -0,0 +1,708 @@
+
+//              Objects:
+// singleton ==>Constructer
+// multi-instance ==>Object Literal
+
+/*
+Syntax:
+        Constructer Method's:
+                Object.create
+*/
+
+// Object Literals:
+/*    
+   Syntax:
+ let jsUser = {}
+ */
+/*
+        Object and Array's similarities and differences:
+/*
+/* Array:
+        myArray = ['Arafat','Yeash',26,true,['Monday','Saturday'],Symbol('key1'),function(){console.log("Hello Arafat");},{},"Fahima",faysal,fahim,null,undefined,NaN,Infinity,-Infinity,0,-0,"",` `,`Js in Bangla`,` ${2+2} `,` ${()=>'Arafat'} `,` ${function(){return 'Arafat'}} `,` ${[1,2,3,4]} `,` ${{name:'Arafat'}} `]
+        log(typeof myArray); // object
+        console.log(myArray[1],[0]); // For Access to Symbol
+
+*/     
+/* Object:
+        myObject = {name:'Arafat',age:26,location:'Demra',isLoggedIn:false,lastLoginDays:['Monday','Saturday'],mySymbol:Symbol('key1'),greeting:function(){console.log("Hello Arafat");}}
+*/
+        
+const jsUser = {
+   /* By Default :
+    0:"Arafat"
+    "name":"Arafat" */
+name:"Arafat",
+"full name":"Al-Arafat Yeash",
+age:26,
+location:"Demra",
+email:"arafatgoogle.com",
+isLoggedIn:false,
+lastLoginDays:["Monday","Saturday"],
+// mySymbol:"myKey1" //Given  as a String input ,not  symbol.
+[mySymbol]:"myKey1"
+}
+const mySymbol = Symbol("key1")
+console.log([mySymbol]); // For Access to Symbol 
+/*
+ Firstly,Define Object...
+  Secondly,Define Symbol
+
+let jsUserObj = {
+==> mySymbol:"myKey1" //Given  as a String input ,not  symbol.
+[mySymbol]:"myKey1"
+const mySymbol = Symbol("key1")
+console.log([mySymbol]);
+}
+
+    That's it to all the process...
+
+Source for "Symbol": 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Data_structures
+ */
+
+
+console.log(jsUser.email);
+// console.log(jsUser[eamil]); //Syntax Error.
+console.log(jsUser["eamil"]);
+// console.log(jsUser.full name); //Got Error...
+console.log(jsUser["full name"]);
+console.log(jsUser["full name"]);
+
+// Change inside the object values/properties.
+jsUser.email = "arfangooglejsUser.com"
+// Object.freeze(jsUser)
+jsUser.email = "arfathfjffhufg.com"
+console.log(jsUser);
+
+//Object Inside of Function:
+jsUser.greeting = function(){
+    console.log("Hello Js User.");
+}
+// console.log(jsUser.greeting); //BecauseOf -->Undefined / [Function( anonymous)]
+// console.log(jsUser.greeting()); // Not a Functions
+jsUser.greetingTwo = function(){
+    console.log(`Hello Js User, ${this.name}`);
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
+
+/*
+Some Unnecessary things:
+// myArray = ["H","i"]
+// myArray[0]
+// () $
+*/
+
+
+const tinderUser = new Object() //Empty Objects ==> This is a "Singleton~Objects."
+/*
+Or,
+const tinderUser = {} //Empty Objects  ==> This is a "Non-Singleton~Objects."
+
+Same Things ,no changes
+*/
+// console.log(tinderUser);
+
+const tinderUser1 = {} // ==>This is a " Non-Singleton~Objects."
+
+tinderUser1.id = " 45374dfjghd"
+tinderUser1.name = "Arafat"
+tinderUser1.isLoggedIn = false
+// console.log(tinderUser1);
+
+console.log(Object.keys(tinderUser1)); //This given Output as an "[] ==>Array."
+console.log(Object.values(tinderUser1));
+console.log(Object.entries(tinderUser1)); //This given Output as an "[] ==>Array in array,each an every single data define as an array,,,like: [[],[],[],[]...]"
+console.log(tinderUser1.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser1.hasOwnProperty('isLogged'));
+
+/*
+Now,
+Objects in Objects: Below...
+*/
+const regularUser = {
+    email:"arafatabc59.com",
+    fullname:{ // objects inside an objects...
+        userfullname:{
+            firstname: " Arafat ",
+            lastname: " Yeash"
+        }
+    }
+}
+/*
+console.log(regularUser);
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.userfullname);
+console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname.userfullname.lastname);
+*/
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"f",4:"k"}
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign(obj1,obj2)
+// const obj3 = Object.assign({},obj1,obj2) // ==>"{}" It is better to use in assign().
+const obj3 = {...obj1,...obj2} //This is use to best...
+// console.log(obj3);
+
+/*
+Object Documentation Source:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+*/
+
+const users = [ // [] ==> This an Array.
+    { // {} ==> This an Objects.
+        id:1,
+        email:"sb1fdsbdf.com"
+    },
+    {
+        id:2,
+        email:"sb2fdsbdf.com"
+    },
+    {
+        id:3,
+        email:"sb3fdsbdf.com"
+    },
+]
+console.log(users);
+console.log(users[1].email);
+// users[1].email
+
+/*
+Browser --> Inspect --> Console :
+const obj1 = {1:"a",2:"b"}
+==> undefined
+==> obj1
+{1: 'a', 2: 'b'}
+1: "a"
+2: "b"
+[[Prototype]]: 
+Objectconstructor: ƒ Object()
+hasOwnProperty: ƒ hasOwnProperty()
+isPrototypeOf: ƒ isPrototypeOf()
+propertyIsEnumerable: ƒ propertyIsEnumerable()
+toLocaleString: ƒ toLocaleString()
+toString: ƒ toString()
+valueOf: ƒ valueOf()
+
+__defineGetter__: ƒ __defineGetter__()
+__defineSetter__: ƒ __defineSetter__()
+__lookupGetter__: ƒ __lookupGetter__()
+__lookupSetter__: ƒ __lookupSetter__()
+
+__proto__: 
+(...)get __proto__: ƒ __proto__()
+set __proto__: ƒ __proto__()
+
+*/
+
+/*
+            Advanced Object(JSON): Object de-structure and JSON API Intro :
+*/
+/*
+     de-structure /De-Structuring occurs 2 Things : i. Array ; ii. Objects
+*/
+/*
+            // Object De-Structure : Same Basic Of React.js ...
+const course = {
+    coursename:"Js in Bangla",
+    price:"999Tk",
+    courseTeacher:"Arafat",
+    courseInstructor:"Yeash"
+
+}
+
+// course.courseTeacher ==> Please don't use,becauseof repeating " course.courseTeacher " writing again and again.
+
+const {courseTeacher} = course  ==> please use this,
+// console.log(courseTeacher);
+
+Or,
+const {courseTeacher:instructor} = course ==>use this.
+// console.log(instructor);
+
+*/
+
+
+/*
+// Some React.js Basic:
+
+const navbar = (props.company) => {
+}
+
+Or,
+
+const navbar = ({company}) => {
+}
+navbar(company = "Arafat")
+
+*/
+
+/*
+Now,
+Some Basic Of " API or JSON " :
+*/
+
+/*
+        // This is an Syntaxical Error,becauseof "Inverted Comas("")" :
+
+{
+    name:"arafat",
+    coursename:"Js Bangla",
+    price:"free"
+}
+
+    // This is an Syntaxically Right,becauseof "Inverted Comas("")" :
+{
+    "name":"arafat",
+    "coursename":"Js Bangla",
+    "price":"free"
+}
+
+*/
+/*
+For API Resource : https://randomuser.me/   
+For API Formate : https://jsonformatter.org/
+*/
+/*
+        // Some API Formate : Get Arrays in Objects,like this : -
+[ // Arrays
+    {},  //objects
+    {},  //objects
+    {}  //objects
+
+]
+*/
+
+/*
+    //  A Example/Model of API/JSON Formate :
+
+{
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Miss",
+        "first": "Jennie",
+        "last": "Nichols"
+      },
+      "location": {
+        "street": {
+          "number": 8929,
+          "name": "Valwood Pkwy",
+        },
+        "city": "Billings",
+        "state": "Michigan",
+        "country": "United States",
+        "postcode": "63104",
+        "coordinates": {
+          "latitude": "-69.8246",
+          "longitude": "134.8719"
+        },
+        "timezone": {
+          "offset": "+9:30",
+          "description": "Adelaide, Darwin"
+        }
+      },
+      "email": "jennie.nichols@example.com",
+      "login": {
+        "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+        "username": "yellowpeacock117",
+        "password": "addison",
+        "salt": "sld1yGtd",
+        "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+        "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+        "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+      },
+      "dob": {
+        "date": "1992-03-08T15:13:16.688Z",
+        "age": 30
+      },
+      "registered": {
+        "date": "2007-07-09T05:51:59.390Z",
+        "age": 14
+      },
+      "phone": "(272) 790-0888",
+      "cell": "(489) 330-2385",
+      "id": {
+        "name": "SSN",
+        "value": "405-88-3636"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/75.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "US"
+    }
+  ],
+  "info": {
+    "seed": "56d27f4a53bd5441",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}
+
+*/
+/*
+                ++++++++++++++++++++++++ X ++++++++++++++++++++++++++
+*/
+
+//              Objects:
+// singleton ==>Constructer
+// multi-instance ==>Object Literal
+
+/*
+Syntax:
+        Constructer Method's:
+                Object.create
+*/
+
+// Object Literals:
+/*    
+   Syntax:
+ let jsUser = {}
+ */
+/*
+        Object and Array's similarities and differences:
+/*
+/* Array:
+        myArray = ['Arafat','Yeash',26,true,['Monday','Saturday'],Symbol('key1'),function(){console.log("Hello Arafat");},{},"Fahima",faysal,fahim,null,undefined,NaN,Infinity,-Infinity,0,-0,"",` `,`Js in Bangla`,` ${2+2} `,` ${()=>'Arafat'} `,` ${function(){return 'Arafat'}} `,` ${[1,2,3,4]} `,` ${{name:'Arafat'}} `]
+        log(typeof myArray); // object
+        console.log(myArray[1],[0]); // For Access to Symbol
+
+*/     
+/* Object:
+        myObject = {name:'Arafat',age:26,location:'Demra',isLoggedIn:false,lastLoginDays:['Monday','Saturday'],mySymbol:Symbol('key1'),greeting:function(){console.log("Hello Arafat");}}
+*/
+// --------------------------------------------------------------------------------
+const jsUser = {
+   
+name:"Arafat",
+"full name":"Al-Arafat Yeash",
+age:26,
+location:"Demra",
+email:"arafatgoogle.com",
+isLoggedIn:false,
+lastLoginDays:["Monday","Saturday"],
+// mySymbol:"myKey1" //Given  as a String input ,not  symbol.
+[mySymbol1]:"myKey1"
+}
+console.log(jsUser.email); // ascessing method 1
+// console.log(jsUser[eamil]); //Syntax Error.
+console.log(jsUser["eamil"]); // ascessing method 2
+// console.log(jsUser.full name); //Got Error...
+console.log(jsUser["full name"]);
+
+const mySymbol1 = Symbol("key1")
+console.log([mySymbol1]); // For Access to Symbol 
+
+
+const mySymbol2 = Symbol("key2")
+jsUser[mySymbol2] = "myKey2"
+console.log(jsUser[mySymbol2]); // For Access to Symbol 2
+
+const mySymbol3 = Symbol("key3")
+const mySymbolic = {
+        mySym :'myKey3.3',
+        [mySymbol3]:"myKey3"
+}
+console.log(mySymbolic[mySymbol3]); // For Access to Symbol 3);
+console.log(typeof mySymbolic[mySymbol3]);
+
+console.log( mySymbolic[mySym]); 
+console.log(typeof mySymbolic[mySym]);
+
+
+/* Changes inside the object values/properties :
+ */
+const myEmail = {
+        email:"arafatgooglejsUser.com"
+}
+myEmail.email = "arfangooglejsUser.com"
+console.log(myEmail);
+console.log(myEmail.email);
+
+// Lock or Freeze the Object :
+const myPassword = {
+       homePassword:"123456789"
+}
+Object.freeze(myPassword)
+myPassword.homePassword = "03414454144"
+console.log(myPassword);
+console.log(myPassword.homePassword); // It will not change,becauseof " Object.freeze() "
+
+//Object Inside of Function:
+myGreeting = function(){
+    console.log("Hello Js User.");
+}
+// Object.freeze(myGreeting)
+console.log(myGreeting);
+console.log(myGreeting()); // Function Calling...
+myEmail.greeting = function(){
+    console.log("Hello Js User.");
+}
+console.log(myEmail.greeting); //BecauseOf -->Undefined / [Function( anonymous)]
+// console.log(myEmail.greeting()); // Not a Functions, beacause of "freeze()"
+
+myEmail.greeting();
+
+myEmail.greetingTwo = function(){
+    console.log(`Hello Js User, ${this.email}`);
+}
+console.log(myEmail.greetingTwo());
+console.log(myEmail.greetingTwo());
+
+// --------------------------------------------------------------------------------
+const tinderUser = new Object() //Empty Objects ==> This is a "Singleton~Objects."
+/*
+Or,
+const tinderUser = {} //Empty Objects  ==> This is a "Non-Singleton~Objects."
+
+Same Things ,no changes
+*/
+// console.log(tinderUser);
+
+const tinderUser1 = {} // ==>This is a " Non-Singleton~Objects."
+
+tinderUser1.id = " 45374dfjghd"
+tinderUser1.name = "Arafat"
+tinderUser1.isLoggedIn = false
+// console.log(tinderUser1);
+
+console.log(Object.keys(tinderUser1)); //This given Output as an "[] ==>Array."
+console.log(Object.values(tinderUser1));
+console.log(Object.entries(tinderUser1)); //This given Output as an "[] ==>Array in array,each an every single data define as an array,,,like: [[],[],[],[]...]"
+console.log(tinderUser1.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser1.hasOwnProperty('isLogged'));
+
+/*
+Now,
+Objects in Objects: Below...
+*/
+const regularUser = {
+    email:"arafatabc59.com",
+    fullname:{ // objects inside an objects...
+        userfullname:{
+            firstname: " Arafat ",
+            lastname: " Yeash"
+        }
+    }
+}
+/*
+console.log(regularUser);
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.userfullname);
+console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname.userfullname.lastname);
+
+console.log(regularUser.fullname?.userfullname.lastname); // Optional Chaining Operator (?.) ;This is used to prevent errors when accessing nested properties of an object that may not exist. For " API data handling ".
+*/
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"f",4:"k"}
+const obj4 = {5:"l",6:"i"}
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign(obj1,obj2)
+// const obj3 = Object.assign({},obj1,obj2,obj4) // ==>"{}" It is better to use in assign().
+//spread operator : 
+const obj3 = {...obj1,...obj2} //This is use to best...   // spreadout
+// console.log(obj3);
+
+/*
+Object Documentation Source:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+*/
+
+const users = [ // [] ==> This an Array.
+    { // {} ==> This an Objects.
+        id:1,
+        email:"sb1fdsbdf.com"
+    },
+    {
+        id:2,
+        email:"sb2fdsbdf.com"
+    },
+    {
+        id:3,
+        email:"sb3fdsbdf.com"
+    },
+]
+console.log(users);
+console.log(users[1].email);
+// users[1].email
+
+/*
+             Some Object's Function :
+Browser --> Inspect --> Console :
+const obj1 = {1:"a",2:"b"}
+==> undefined
+==> obj1
+{1: 'a', 2: 'b'}
+1: "a"
+2: "b"
+[[Prototype]]: Object
+1)constructor: Object()
+2)hasOwnProperty: hasOwnProperty()
+3)isPrototypeOf: isPrototypeOf()
+4)propertyIsEnumerable: propertyIsEnumerable()
+5)toLocaleString: toLocaleString()
+6)toString: toString()
+7)valueOf: valueOf()
+
+i)__defineGetter__: __defineGetter__()
+ii)__defineSetter__: __defineSetter__()
+iii)__lookupGetter__: __lookupGetter__()
+iv)__lookupSetter__: __lookupSetter__()
+
+__proto__: (...)
+
+a)get __proto__: __proto__()
+b)set __proto__: __proto__()
+
+*/
+
+/*
+            // Object De-Structure : Same Basic Of React.js ...
+const course = {
+    coursename:"Js in Bangla",
+    price:"999Tk",
+    courseTeacher:"Arafat",
+    courseInstructor:"Yeash"
+
+}
+course.courseTeacher
+// "course.courseTeacher" ==> Please don't use this Term or Methods,becauseof repeating " course.courseTeacher " writing again and again. 
+// Insead of this :below...
+
+const {courseTeacher} = course  ==> please use this,
+// console.log(courseTeacher);
+
+Or,
+const {courseTeacher:teacher} = course ==>use this.
+// console.log(teacher);
+*/
+
+
+/*
+// Some React.js Basic:
+
+const navbar = (props.company) //props means property => {
+}
+navbar(company = "Arafat")
+Or,
+
+const navbar = ({company}) => {
+}
+navbar(company = "Arafat")
+
+*/
+
+/*
+Now,
+Some Basic Of " API or JSON " :
+*/
+
+/*
+        // This is an Syntaxical Error,becauseof "Inverted Comas("")" :
+
+{
+    name:"arafat",
+    coursename:"Js Bangla",
+    price:"free"
+}
+
+    // This is an Syntaxically Right,becauseof "Inverted Comas("")" :
+{
+    "name":"arafat",
+    "coursename":"Js Bangla",
+    "price":"free"
+}
+
+*/
+/*
+For API Resource : https://randomuser.me/   
+For API Formate : https://jsonformatter.org/
+*/
+/*
+        // Some API Formate : Get Arrays in Objects,like this : -
+[ // Arrays
+    {},  //objects
+    {},  //objects
+    {}  //objects
+
+]
+*/
+
+/*
+    //  A Example/Model of API/JSON Formate :
+
+{
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Miss",
+        "first": "Jennie",
+        "last": "Nichols"
+      },
+      "location": {
+        "street": {
+          "number": 8929,
+          "name": "Valwood Pkwy",
+        },
+        "city": "Billings",
+        "state": "Michigan",
+        "country": "United States",
+        "postcode": "63104",
+        "coordinates": {
+          "latitude": "-69.8246",
+          "longitude": "134.8719"
+        },
+        "timezone": {
+          "offset": "+9:30",
+          "description": "Adelaide, Darwin"
+        }
+      },
+      "email": "jennie.nichols@example.com",
+      "login": {
+        "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+        "username": "yellowpeacock117",
+        "password": "addison",
+        "salt": "sld1yGtd",
+        "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+        "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+        "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+      },
+      "dob": {
+        "date": "1992-03-08T15:13:16.688Z",
+        "age": 30
+      },
+      "registered": {
+        "date": "2007-07-09T05:51:59.390Z",
+        "age": 14
+      },
+      "phone": "(272) 790-0888",
+      "cell": "(489) 330-2385",
+      "id": {
+        "name": "SSN",
+        "value": "405-88-3636"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/75.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "US"
+    }
+  ],
+  "info": {
+    "seed": "56d27f4a53bd5441",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}
+
+*/
